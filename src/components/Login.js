7112852d46c6c21e4e4846c84f978414a5cdc37d
Muhammad Talha Baig <57634631+mtalhabaig3@users.noreply.github.com>
2023-06-05 00:02:15 +0500

    implemented login screen

---
+++ src/components/Login.js
@@ -0,0 +1,45 @@
+// Login.js
+import React, { useState } from "react";
+
+const Login = ({ onLogin }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleLogin = () => {
+    // Retrieve user data from local storage
+    const users = JSON.parse(localStorage.getItem("users")) || [];
+    const user = users.find(
+      (u) => u.email === email && u.password === password
+    );
+
+    if (user) {
+      setError("");
+      onLogin(user.id);
+    } else {
+      setError("Invalid email or password.");
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <input
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="Email"
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="Password"
+      />
+      <button onClick={handleLogin}>Login</button>
+      {error && <p>{error}</p>}
+    </div>
+  );
+};
+
+export default Login;
